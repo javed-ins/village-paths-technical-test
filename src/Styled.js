d6b3a424313435e03b1fcb7e61a9f727b7299ee2
javed-ins <javed@intangible-solutions.com>
2021-09-27 20:08:52 +0500

    new account screen implementation by using antd

--- src/Styled.js
+++ src/Styled.js
@@ -1,7 +1,40 @@
-import { Menu } from 'antd';
+import { Image, Menu, Col, Button } from 'antd';
 import styled from 'styled-components';
 
+export const CardImage = styled(Image)`
+  margin-top: 4rem;
+`;
+
+export const LayoutCol = styled(Col)`
+  margin-top: 4rem;
+`;
+
+export const LeftArrow = styled(Image)`
+  padding: 0.5rem;
+  border-radius: 50%;
+  border: 1px solid lightGrey;
+`;
 export const HeaderStyled = styled(Menu)`
   padding-left: 1.5em;
   padding-top: 1.5rem;
 `;
+
+export const ImageCol = styled(Col)`
+  height: 93vh;
+  text-align: center;
+  background: #f5f5f6;
+`;
+
+export const StyledButton = styled(Button)(
+  ({ secondary }) => `
+color: ${secondary ? '#494A4E' : '#FFFFFF'};
+padding: .5rem 1rem;
+height: 40px;
+text-align: center;
+background-color: ${secondary ? '#FFFFFF' : '#2F5082'}; 
+border-radius: 8px;
+font-size: 16px;
+font-weight: 600;
+line-height: 16px;
+`,
+);
--- src/components/newAccount.js
+++ src/components/newAccount.js
@@ -1,5 +1,100 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Form, Input, Row, Col, Typography, Space } from 'antd';
+import {
+  CardImage,
+  LayoutCol,
+  LeftArrow,
+  StyledButton,
+  ImageCol,
+} from '../Styled';
 
-export default function newAccount() {
-  return <div>New Account</div>;
+import LeftArrowIcon from '../assets/svg/arrow-left.svg';
+import UseCard from '../assets/svg/user-cards.svg';
+
+export default function NewAccount() {
+  const { Title, Text } = Typography;
+  const [email, setEmail] = useState('');
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [password, setPassword] = useState('');
+  return (
+    <React.Fragment>
+      <Row>
+        <LayoutCol span={13}>
+          <Row>
+            <Col sm={{ span: 23, offset: 1 }} md={{ span: 1, offset: 1 }}>
+              <LeftArrow src={LeftArrowIcon} />
+            </Col>
+            <Col sm={{ span: 22, offset: 1 }} md={{ span: 20, offset: 2 }}>
+              <React.Fragment>
+                <Space direction="vertical">
+                  <Title level={2}>Let's get to know you...</Title>
+                  <Text>Tell us more about yourself.</Text>
+                </Space>
+                <Row>
+                  <Col xs={24} sm={24} md={15} lg={13}>
+                    <Form layout="vertical" name="basic" size="large">
+                      <Row>
+                        <Col span="24">
+                          <Form.Item
+                            style={{ padding: '9px 0 0', borderRadius: '5px' }}
+                            name="email"
+                          >
+                            <Input
+                              placeholder="anita.village@villagecreed.com"
+                              value={email}
+                              onChange={(e) => setEmail(e.target.value)}
+                            />
+                          </Form.Item>
+                        </Col>
+                      </Row>
+                      <Row justify="space-between">
+                        <Col span="12">
+                          <Form.Item
+                            name="First Name"
+                            style={{ borderRadius: '6px' }}
+                          >
+                            <Input
+                              placeholder="Anita"
+                              value={firstName}
+                              onChange={(e) => setFirstName(e.target.value)}
+                            />
+                          </Form.Item>
+                        </Col>
+                        <Col span="11">
+                          <Form.Item
+                            name="Last Name"
+                            style={{ borderRadius: '6px' }}
+                          >
+                            <Input
+                              placeholder="Village"
+                              value={lastName}
+                              onChange={(e) => setLastName(e.target.value)}
+                            />
+                          </Form.Item>
+                        </Col>
+                      </Row>
+                      <Form.Item name="password">
+                        <Input.Password
+                          placeholder="Password"
+                          value={password}
+                          onChange={(e) => setPassword(e.target.value)}
+                        />
+                      </Form.Item>
+                      <Form.Item>
+                        <StyledButton htmlType="submit">Continue</StyledButton>
+                      </Form.Item>
+                    </Form>
+                  </Col>
+                </Row>
+              </React.Fragment>
+            </Col>
+          </Row>
+        </LayoutCol>
+        <ImageCol sm={24} md={24} lg={11} span={11}>
+          <CardImage preview={false} src={UseCard} alt="UseCard Image" />
+        </ImageCol>
+      </Row>
+    </React.Fragment>
+  );
 }
