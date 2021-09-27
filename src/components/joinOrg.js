ea6d5887332cc6676fbf482f2fd140429f45a519
javed-ins <javed@intangible-solutions.com>
2021-09-27 20:40:00 +0500

    joinOrg screen and navigation between screen

--- src/components/joinOrg.js
+++ src/components/joinOrg.js
@@ -1,5 +1,81 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Image, Row, Col, Typography, Card, Space } from 'antd';
+import {
+  CardImage,
+  LayoutCol,
+  LeftArrow,
+  StyledButton,
+  ImageCol,
+} from '../Styled';
 
-export default function joinOrg() {
-  return <div>Join Organization</div>;
+import LeftArrowIcon from '../assets/svg/arrow-left.svg';
+import JoinOrgImg from '../assets/svg/join-org.svg';
+import SmallLogo from '../assets/svg/logo.svg';
+import { useHistory } from 'react-router-dom';
+
+import { PlusOutlined } from '@ant-design/icons';
+
+export default function JoinOrg() {
+  const history = useHistory();
+  const { Title, Text } = Typography;
+  return (
+    <React.Fragment>
+      <Row>
+        <LayoutCol span={13}>
+          <Row>
+            <Col sm={{ span: 23, offset: 1 }} md={{ span: 1, offset: 1 }}>
+              <LeftArrow
+                preview={false}
+                src={LeftArrowIcon}
+                onClick={() => {
+                  history.goBack();
+                }}
+              />
+            </Col>
+            <Col sm={{ span: 22, offset: 1 }} md={{ span: 20, offset: 2 }}>
+              <React.Fragment>
+                <Space direction="vertical">
+                  <Title level={2}>Join your organization in Village Hub</Title>
+                  <Col sm={22} md={20} style={{ paddingBottom: '2rem' }}>
+                    <Text>
+                      This organization allow anyone from
+                      <b>@villagecreed.com </b> to join:
+                    </Text>
+                  </Col>
+                </Space>
+                <Col sm={22} md={16}>
+                  <Card>
+                    <Row align="middle">
+                      <Col span="4">
+                        <Image preview={false} width={70} src={SmallLogo} />
+                      </Col>
+                      <Col span="16">
+                        <Space direction="vertical">
+                          <Title level={5}>
+                            <b>Village Creed PBC</b>
+                          </Title>
+                          <Text style={{ color: 'grey' }}>11 members</Text>
+                        </Space>
+                      </Col>
+                      <Col span="4">
+                        <StyledButton>Join</StyledButton>
+                      </Col>
+                    </Row>
+                  </Card>
+                </Col>
+                <Space direction="vertical" style={{ padding: '1.5rem 0' }}>
+                  <StyledButton icon={<PlusOutlined />} secondary type="dashed">
+                    Create a new organizaton
+                  </StyledButton>
+                </Space>
+              </React.Fragment>
+            </Col>
+          </Row>
+        </LayoutCol>
+        <ImageCol sm={24} md={24} lg={11} span={11}>
+          <CardImage preview={false} src={JoinOrgImg} alt="Join Org Image" />
+        </ImageCol>
+      </Row>
+    </React.Fragment>
+  );
 }
--- src/components/newAccount.js
+++ src/components/newAccount.js
@@ -10,8 +10,10 @@ import {
 
 import LeftArrowIcon from '../assets/svg/arrow-left.svg';
 import UseCard from '../assets/svg/user-cards.svg';
+import { useHistory } from 'react-router-dom';
 
 export default function NewAccount() {
+  const history = useHistory();
   const { Title, Text } = Typography;
   const [email, setEmail] = useState('');
   const [firstName, setFirstName] = useState('');
@@ -23,7 +25,13 @@ export default function NewAccount() {
         <LayoutCol span={13}>
           <Row>
             <Col sm={{ span: 23, offset: 1 }} md={{ span: 1, offset: 1 }}>
-              <LeftArrow src={LeftArrowIcon} />
+              <LeftArrow
+                preview={false}
+                src={LeftArrowIcon}
+                onClick={() => {
+                  history.goBack();
+                }}
+              />
             </Col>
             <Col sm={{ span: 22, offset: 1 }} md={{ span: 20, offset: 2 }}>
               <React.Fragment>
@@ -32,13 +40,21 @@ export default function NewAccount() {
                   <Text>Tell us more about yourself.</Text>
                 </Space>
                 <Row>
-                  <Col xs={24} sm={24} md={15} lg={13}>
-                    <Form layout="vertical" name="basic" size="large">
+                  <Col sm={24} md={14} lg={13}>
+                    <Form
+                      layout="vertical"
+                      name="basic"
+                      size="large"
+                      onFinish={(email) => {
+                        history.push('/joinOrg');
+                      }}
+                    >
                       <Row>
                         <Col span="24">
                           <Form.Item
                             style={{ padding: '9px 0 0', borderRadius: '5px' }}
                             name="email"
+                            rules={[{ required: true }]}
                           >
                             <Input
                               placeholder="anita.village@villagecreed.com"
@@ -53,6 +69,7 @@ export default function NewAccount() {
                           <Form.Item
                             name="First Name"
                             style={{ borderRadius: '6px' }}
+                            rules={[{ required: true }]}
                           >
                             <Input
                               placeholder="Anita"
@@ -65,6 +82,7 @@ export default function NewAccount() {
                           <Form.Item
                             name="Last Name"
                             style={{ borderRadius: '6px' }}
+                            rules={[{ required: true }]}
                           >
                             <Input
                               placeholder="Village"
@@ -74,7 +92,7 @@ export default function NewAccount() {
                           </Form.Item>
                         </Col>
                       </Row>
-                      <Form.Item name="password">
+                      <Form.Item name="password" rules={[{ required: true }]}>
                         <Input.Password
                           placeholder="Password"
                           value={password}
